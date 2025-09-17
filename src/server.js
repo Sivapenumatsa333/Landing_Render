ef470983b32a89d16e682edf4aae5156f5092410
Sivapenumatsa333 <88231439+Sivapenumatsa333@users.noreply.github.com>
2025-09-17 18:06:28 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: ORIGIN === "*" ? true : ORIGIN,
     credentials: true,
   })
 );
